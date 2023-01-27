7811650a75fc97ce93f372bb97d3050c319e18e5
akashyap25 <anuragkashyap026@gmail.com>
2023-01-27 17:46:01 +0530

    added tracks

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './Hero'
 import Hero from './Hero'
 import About from './About'
 import Home from './Home'
+import Tracks from './Tracks'
 const App = () => {
   return (
     <div className='bg-newcolor'>
     
      <Home />
+     <Tracks/>
       </div>
   )
 }
