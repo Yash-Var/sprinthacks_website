2c7f5b0d70cac264349912b86106ccba4fc329f0
Yash Varshney <yashvarshney@pop-os.localdomain>
2023-01-27 15:35:09 +0530

    yash

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './Hero'
 import Hero from './Hero'
 import About from './About'
 import Home from './Home'
+import Contact from './Contact'
+
 const App = () => {
   return (
     <div className='bg-newcolor'>
     
      <Home />
+    <Contact />
+    {/* <Track /> */}
       </div>
   )
 }
---
+++ src/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import frame from './images/Frame 9.png'
+import instagram from './images/instagram.png'
+import linkedin from './images/linkedin.png'
+import twitter from './images/twitter.png'
+import mail from './images/mail.png'
+
+const Contact = () => {
+  return (
+    <div className='bg-newcolor flex justify-center p-10 space-x-4'>
+        <h1 className='text-white main text-3xl'>Contact us at:</h1>
+<a href='https://www.instagram.com/dsckiet/'><img src={instagram} /></a>
+<a href='https://twitter.com/dsckiet'><img src={twitter} /></a>
+<a href='https://www.linkedin.com/company/dsckiet/mycompany/'><img src={linkedin} /></a>
+<a href='mailto:dsckiet@gmail.com'><img src={mail} /></a>
+    </div>
+  )
+}
+
+export default Contact
