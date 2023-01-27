f9cb8111286a5bd17fc8b886b3e007cb665888da
Ujjwal-soni98 <ujjwal.soniji@gmail.com>
2023-01-27 15:27:57 +0530

    about page half done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './Hero'
+import Hero from './Hero'
+import About from './About'
+import Home from './Home'
+const App = () => {
+  return (
+    <div className='bg-newcolor'>
+    
+     {/* <Home /> */}
+     <About />
+      </div>
+  )
+}
+
+export default App
