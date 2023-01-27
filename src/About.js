1e429c58456cf1422184fc812b5c991cce1a6ac4
Ujjwal-soni98 <ujjwal.soniji@gmail.com>
2023-01-27 11:55:27 +0530

    about page

--- src/About.js
+++ src/About.js
@@ -8,8 +8,10 @@ const About = () => {
             <h1 className='main text-center text-7xl text-white uppercase p-8'>About us</h1>
         </div>
 <div className='relative'>
+      <ul>
     <img src={ellipse3} alt="absolute z-10" />
     <img src={ellipse} alt="absolute z-0 -top-4 right-2" />
+    </ul>
         </div>
 
     </div>
