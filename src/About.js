2e1fec01931dcbfa30847923bdd0364717b8a929
Ujjwal-soni-98 <ujjwal.soniji@gmail.com>
2023-01-28 19:37:20 +0530

    added all section

--- src/About.js
+++ src/About.js
@@ -8,21 +8,11 @@ const About = () => {
         <div>
             <h1 className='main text-center text-7xl text-white uppercase p-8'>About us</h1>
         </div>
-<<<<<<< Updated upstream
         <div className='flex justify-between items-center'>
         <div className='relative'>
           <div className='flex'>
             <img src={ellipse} className='w-[56rem] h-[64rem]'  />
             <img src={person} className='w-[500px] h-[570px] absolute top-48 left-80'/>
-=======
-        <div className='flex justify-between'>
-        <div className='relative -top-40'>
-          <div>
-            <img src={ellipse} className='' />
-          </div>
-          <div className='absolute top-56 left-28'>
-            <img src={person} className='w-[400px] h-[570px]'/>
->>>>>>> Stashed changes
           </div>
           
         </div>
--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import Navbar from './Navbar'
-import './Hero'
-import Hero from './Hero'
+import Contact from './Contact'
+import Sponsors from './Sponsors'
+import Faq from './Faq'
+import Tracks from './Tracks'
 import About from './About'
 import Home from './Home'
+import Prize from './Prize'
+import Themes from './Themes'
 const App = () => {
   return (
     <div className='bg-newcolor'>
     
-     {/* <Home /> */}
+     <Home />
      <About />
+     <Themes />
+     <Prize />
+     <Tracks />
+     <Faq />
+     <Sponsors />
+     <Contact />
+
       </div>
   )
 }
+
+export default App
