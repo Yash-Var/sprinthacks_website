bc7b6b2d7a0e3dfcab65277187d17b64b8c45ff4
Ujjwal-soni-98 <ujjwal.soniji@gmail.com>
2023-01-28 15:07:15 +0530

    faq

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from "react";
-import About from "./About";
-import Prize from "./Prize";
-import Home from "./Home";
-import Contact from './Contact'
-import Sponsors from './Sponsors'
-import Tracks from './Tracks'
-import Faq from "./Faq";
-
-
+import React from 'react'
+import Navbar from './Navbar'
+import './Hero'
+import Hero from './Hero'
+import About from './About'
+import Home from './Home'
 const App = () => {
   return (
     <div className='bg-newcolor'>
     
-     <Home />
-<About />
-<Prize/>
-<Tracks />
-<Faq />
-<Sponsors />
-<Contact />
-    </div>
-  );
-};
-
-
-export default App;
+     {/* <Home /> */}
+     <About />
+      </div>
+  )
+}
