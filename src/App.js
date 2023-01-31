ea41afd47a57b3e82d96644e16695c7fc1b93d09
Yash-Var <yashvarshney7011@gmail.com>
2023-01-31 20:28:37 +0530

    added the app.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Themes from "./Themes";
 import Hero from "./Hero";
 const App = () => {
   return (
-    <div className="bg-newcolor">
+    <div className="bg-newcolor overflow-hidden">
       {/* <Navbar2 /> */}
       {/* <Navbar /> */}
       <Home />
@@ -23,6 +23,17 @@ const App = () => {
       <Tracks />
       <Faq />
       <Sponsors />
+      <div>
+        <center>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13991.494256086988!2d77.47975453955078!3d28.753191700000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf574d18f2b6f%3A0x4a65c0bc0122eb2f!2sKIET%20Group%20of%20Institutions!5e0!3m2!1sen!2sin!4v1675160744598!5m2!1sen!2sin"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            className="w-[80vw] h-[30vw] p-7 "
+          ></iframe>
+        </center>
+      </div>
       <div className="meet flex justify-center">
         <a href="https://dsckiet.com/team">
           <h1 className="text-blue-800 main text-2xl">Meet our team</h1>
