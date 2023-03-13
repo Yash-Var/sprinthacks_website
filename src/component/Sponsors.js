8812d01979a04c604fa85b28046bc1ee66c03994
Harshit Rai <96918819+Harshitr10@users.noreply.github.com>
2023-03-13 18:05:49 +0530

    Update Sponsors.js

--- src/component/Sponsors.js
+++ src/component/Sponsors.js
@@ -10,12 +10,12 @@ import filecoin from "../assests/images/filecoin.png";
 const Sponsors = () => {
   return (
     <div className="bg-newcolor" id="sponsor">
-      <div className="flex justify-center pt-16 pb-10  max-[415px]:pb-5 max-[415px]:ml-5">
+      <div className="flex justify-center pt-16 pb-10  max-[415px]:pb-5 max-[415px]:ml-5 max-[415px]:py-5">
         <h1 className="text-white text-7xl main max-[500px]:text-6xl max-[415px]:text-5xl prizes_">
           SPONSORS
         </h1>
       </div>
-      <div className="flex pt-16 pb-10 justify-around max-[415px]:pb-5 max-[415px]:pt-5">
+      <div className="flex pt-10 pb-10 justify-around max-[415px]:py-3">
         <img
           src={Line}
           className="h-2 mt-11 mx-10 overflow-hidden max-md:mx-3 max-[500px]:mt-9 max-[454px]:mt-14 max-[415px]:hidden "
@@ -28,17 +28,17 @@ const Sponsors = () => {
           className="h-2 mt-11 mx-10 overflow-hidden max-md:mx-3 max-[500px]:mt-9 max-[454px]:mt-14 max-[415px]:hidden"
         />
       </div>
-      <div className=" flex  flex-wrap justify-center pt-10 pb-10 ">
+      <div className=" flex  flex-wrap justify-center pt-10 pb-10  max-[415px]:py-5">
         <a href="https://devfolio.co/" target="_blank">
           <button
             type="button"
             className="bg-[#273339] rounded-lg  px-20 py-5 mr-3 my-5"
           >
             <div className="flex space-x-2">
-              <div>
+              <div className="max-[390px]:w-10">
                 <img src={Devfolio} />
               </div>
-              <h1 className="text-4xl font-bold Sponsors pt-1 text-white">
+              <h1 className="text-4xl font-bold Sponsors pt-1 text-white max-[415px]:text-3xl max-[390px]:text-2xl">
                 Devfolio
               </h1>
             </div>
@@ -51,17 +51,17 @@ const Sponsors = () => {
             className="bg-[#273339] rounded-lg  py-5 px-24 mr-2 my-5"
           >
             <div className="flex space-x-2">
-              <div>
+              <div className="max-[390px]:w-10">
                 <img src={Polygon} />
               </div>
-              <h1 className="text-3xl font-bold  Sponsors navbar pt-1 text-white">
+              <h1 className="text-3xl font-bold  Sponsors navbar pt-1 text-white max-[415px]:text-2xl max-[390px]:text-xl">
                 polygon
               </h1>
             </div>
           </button>
         </a>
       </div>
-      <div className="flex pt-10 pb-10 justify-evenly max-[415px]:pt-2">
+      <div className="flex pt-10 pb-10 justify-evenly max-[415px]:py-3">
         <img
           src={Line}
           className="h-2 mt-12 mx-10 overflow-hidden max-md:mx-3 max-[500px]:mt-10 max-[454px]:mt-14 max-[415px]:hidden"
@@ -74,7 +74,7 @@ const Sponsors = () => {
           className="h-2 mt-11 mx-10 overflow-hidden max-md:mx-3 max-[500px]:mt-10 max-[454px]:mt-14 max-[415px]:hidden"
         />
       </div>
-      <div className="flex  flex-wrap justify-center pt-10 pb-10 ">
+      <div className="flex  flex-wrap justify-center pt-10 pb-10 max-[415px]:pt-5 max-[415px]:pb-10">
         <a href="https://solana.com/" target="_blank">
           <button
             type="button"
@@ -92,11 +92,11 @@ const Sponsors = () => {
             className="bg-[#273339] rounded-lg  px-28 py-6 ml-3 mr-6 my-5"
           >
             <div className="flex space-x-1">
-              <div>
+              <div >
                 <img src={Replit1} />
               </div>
-              <h1 className="text-2xl Sponsors pt-1 text-white">replit</h1>
-              <div>
+              <h1 className="text-2xl Sponsors pt-1 text-white max-[390px]:text-xl">replit</h1>
+              <div >
                 <img src={Replit2} />
               </div>
             </div>
@@ -106,10 +106,10 @@ const Sponsors = () => {
         <a href="https://filecoin.io/" target="_blank">
           <button
             type="button"
-            className="bg-[#273339]  rounded-lg  px-24 py-5 mr-2 my-5 max-[415px]:pb-1"
+            className="bg-[#273339]  rounded-lg  px-24 py-5 mr-2 my-5 max-[415px]:pb-4"
           >
             <div className="flex space-x-2">
-              <div>
+              <div className="max-[390px]:w-10">
                 <img src={filecoin} />
               </div>
               <h1 className="text-2xl font-bold Sponsors pt-2 text-white">
