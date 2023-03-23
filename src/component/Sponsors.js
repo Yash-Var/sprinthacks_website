4fecb8797a01179bfcf14a6cae01290bc11d0d19
Harshit Rai <96918819+Harshitr10@users.noreply.github.com>
2023-03-23 23:40:49 +0530

    Update Sponsors.js

--- src/component/Sponsors.js
+++ src/component/Sponsors.js
@@ -67,7 +67,7 @@ const Sponsors = () => {
         <a href="https://greedygame.com/" target="_blank">
           <button
             type="button"
-            className="bg-[#273339] rounded-lg  py-4 px-20 mr-2 my-5  max-[520px]:px-16 max-[415px]:px-12   "
+            className="bg-[#273339] rounded-lg  py-4 px-20 mr-2 my-5 max-[520px]:px-16 max-[415px]:px-12   "
           >
             <div className="flex space-x-2">
               <div>
@@ -122,7 +122,7 @@ const Sponsors = () => {
         <a href="https://filecoin.io/" target="_blank">
           <button
             type="button"
-            className="bg-[#273339]  rounded-lg  px-24 py-5 mr-2 my-5 max-[415px]:pb-5 max-[390px]:px-20"
+            className="bg-[#273339]  rounded-lg  px-24 py-5 mr-2 my-3 max-[415px]:pb-5 max-[390px]:px-20"
           >
             <div className="flex space-x-2">
               <div className="max-[390px]:w-10">
@@ -176,13 +176,13 @@ const Sponsors = () => {
           </button>
         </a>
       </div>
-      <div className="flex pt-10 pb-10 justify-around max-[415px]:py-3">
+      <div className="flex pt-10 pb-10 justify-around max-[415px]:py-7">
         {/* <img
           src={Line}
           className="h-2 mt-11 mx-10 overflow-hidden max-md:mx-3 max-[500px]:mt-9 max-[454px]:mt-14 max-[415px]:hidden "
         /> */}
         <center>
-          <h1 className="text-white text-6xl main max-[500px]:text-5xl max-[466px]:mr-5 ">
+          <h1 className="text-white text-6xl main max-[600px]:text-5xl max-[466px]:mr-5 ">
             COMMUNITY PARTNER
           </h1>
         </center>
@@ -192,19 +192,7 @@ const Sponsors = () => {
         /> */}
       </div>
       <div className=" flex  flex-wrap justify-center pt-10 pb-10  max-[415px]:py-5 ">
-        {/* <a href="https://devfolio.co/" target="_blank">
-          <button
-            type="button"
-            className="bg-[#273339] rounded-lg  px-20 py-5 mr-3 my-5 max-[520px]:px-16 "
-          >
-            <div className="flex space-x-2">
-              <div className="max-[390px]:w-10">
-                <img src={Foss} className="w-56"/>
-              </div>
-             
-            </div>
-          </button>
-        </a> */}
+      
 
         <a href="https://www.threeway.studio/" target="_blank">
           <button
@@ -219,14 +207,14 @@ const Sponsors = () => {
           </button>
         </a>
       </div>
-      <div className="flex pt-10 pb-10 justify-around max-[415px]:py-3">
+      <div className="flex pt-10 pb-10 justify-around max-[415px]:py-7">
         {/* <img
           src={Line}
           className="h-2 mt-11 mx-10 overflow-hidden max-md:mx-3 max-[500px]:mt-9 max-[454px]:mt-14 max-[415px]:hidden "
         /> */}
         <center>
-          <h1 className="text-white text-6xl main max-[500px]:text-5xl max-[466px]:mr-5 ">
-            CERTIFICATE SPONSOR
+          <h1 className="text-white text-6xl main max-[600px]:text-5xl max-[466px]:mr-5 ">
+            CERTIFICATE PARTNER
           </h1>
         </center>
         {/* <img
@@ -238,11 +226,11 @@ const Sponsors = () => {
         <a href="https://givemycertificate.com/" target="_blank">
           <button
             type="button"
-            className="bg-[#273339] rounded-lg  px-20 py-5 mr-3 my-5 max-[520px]:px-16 "
+            className="bg-[#273339] rounded-lg  px-20 py-5 mr-3 my-5 max-[520px]:px-16 max-[389px]:pb-1 max-[389px]:px-10 "
           >
             <div className="flex space-x-2">
-              <div className="max-[390px]:w-10">
-                <img src={GMC} className="h-36" />
+              <div className="max-[389px]:h-32">
+                <img src={GMC} className="h-36 max-[389px]:h-28" />
               </div>
             </div>
           </button>
