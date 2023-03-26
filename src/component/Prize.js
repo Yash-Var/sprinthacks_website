ab33605f3a2d7b7d226010067435c3a47d7beb65
CoderRaj <84132589+GeekCoderRaj@users.noreply.github.com>
2023-03-26 05:53:07 +0000

    --prize_section_styling_change--

--- src/component/Prize.js
+++ src/component/Prize.js
@@ -10,12 +10,14 @@ function Prize() {
       </div>
 
       <div className="row flex justify-center m-2 mb-16 prize">
-        <div className="prize2 text-white uppercase abouttext text-center">
+        <div className="prize2 text-white uppercase abouttext text-center 
+">
           <img
             border="0"
             src="https://i.ibb.co/SVWswxW/prize2.png"
             alt="prize2"
-            className="m-8 mt-20"
+            className="my-16 h-48 m-auto prize_mobile_screen
+            "
           />
           <h1 className="navbar text-6xl">1ST RUNNER UP</h1>
           <h1 className="navbar text-5xl mt-4">₹20,000</h1>
@@ -25,17 +27,18 @@ function Prize() {
             src="https://i.ibb.co/N7xD1pX/prize1.png"
             alt="prize1"
             border="0"
-            className="m-8 image_one"
+            className=" my-4 m-auto h-60 image_one"
           />
           <h1 className="navbar text-6xl tracking-widest winner"> WINNER</h1>
           <h1 className="navbar text-5xl mt-4">₹30,000</h1>
         </div>
-        <div className="prize3 text-white uppercase abouttext text-center ">
+        <div className="prize3  text-white uppercase abouttext text-center ">
           <img
             src="https://i.ibb.co/fFnpKtJ/prize3.png"
             alt="prize3"
             border="0"
-            className="m-8 mt-24"
+            className="mt-22 my-16 h-48 m-auto prize_mobile_screen
+            "
           />
           <h1 className="navbar text-6xl">2ND RUNNER UP</h1>
           <h1 className="navbar text-5xl mt-4">₹10,000</h1>
